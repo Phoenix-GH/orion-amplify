92f6a71d2a595006bb3d5de3568e929141385d64
Phoenix <phoenix214@gmail.com>
2018-04-15 22:42:13 -0400

    Fix on #26

--- src/stories/screens/SubmitNewIR/SelectRule.tsx
+++ src/stories/screens/SubmitNewIR/SelectRule.tsx
@@ -1,3 +1,4 @@
+/* K2 */
 import * as React from "react";
 import { Dimensions } from 'react-native';
 import {
@@ -21,29 +22,41 @@ export interface Props {
 export interface State {}
 const deviceWidth = Dimensions.get('window').width;
 
+const customItem = {
+	RuleReference: '',
+	SuggestedResolution: '',
+	Name: ''
+};
+
 class SelectNewIRStage extends React.Component<Props, State> {
+	
 	render() {
-		const { matchData, squaddingData, ruleViolation } = this.props;
+		const { matchData, squaddingData, ruleViolation, changePage } = this.props;
 		return (
 			<Content style={{width: deviceWidth}}>
 				<H2>{squaddingData && squaddingData.Participant.DisplayName}</H2>
 				<List>
 					{
 						matchData && matchData.SquaddingEvents && matchData.CommonIncidentReports.map((item, i) => (
-							<ListItem
-								key={i}
-								onPress={() => this.props.changePage(2, item)}
-							>
-								<Left>
-									<Text>{item.Name}</Text>
-									<Text note>{item.RuleReference}</Text>
-								</Left>
-								<Right>
-									<Radio selected={ruleViolation && ruleViolation.RuleReference === item.RuleReference} />
-								</Right>
-							</ListItem>
+						<ListItem
+							key={i}
+							onPress={() => changePage(2, item)}
+						>
+							<Left>
+								<Text>{item.Name}</Text>
+								<Text note>{item.RuleReference}</Text>
+							</Left>
+							<Right>
+								<Radio selected={ruleViolation && ruleViolation.RuleReference === item.RuleReference} />
+							</Right>
+						</ListItem>
 						))
 					}
+					<ListItem key='custom' onPress={() => changePage(2, customItem)}>
+						<Left>
+							<Text>Custom Incident Report</Text>
+						</Left>
+					</ListItem>
 				</List>
 			</Content>
 		);
